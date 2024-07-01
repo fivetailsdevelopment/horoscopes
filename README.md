18e94ffe7ab1b5cad8efd2dac3c375b346e647c2
fivetailsdevelopment <jhohnen@yahoo.com>
2024-07-01 11:25:41 +1000

    first commit

---
+++ README.md
@@ -0,0 +1,12 @@
+# Horoscope Generator
+by Five Tails Design
+
+## Project scope
++ Generate horoscopes for the 12 Zodiac Signts
++ Four components to each horoscope:
+    + Star sign
+    + Something in retrograde
+    + You will experience...
+    + Be on the look out for...
+
+    
---
+++ horoscopes.js
@@ -0,0 +1,6 @@
+/* Arrays for the four components of the horoscope */
+const starSign = ['Ares', 'Taurus', 'Gemini', 'Cancer', 'Leo', 'Virgo', 'Libra', 'Scorpio', 'Sagittarius', 'Capricorn', 'Aquarius', 'Pisces'];
+const retrograde = ['moon', 'Mercury', 'Venus', 'Saturn', 'Jupiter'];
+const willExperience = ['rapturous joy', 'unquenchable curiosity', 'boundless energy', 'bottomless hunger', 'relentless fatigue'];
+const onTheLookoutFor = ['gnomes wearing kilts', 'sailors dancin to the YMCA', 'carnivorous butterflies', "Donald Trump's ego"];
+
